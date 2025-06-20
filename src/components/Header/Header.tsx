3e1f95a93c9430bd7a68218e2872e4e0b34f841b
xfja <franjimenezaguilera@gmail.com>
2025-06-20 11:51:34 +0200

    feat: add icons to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,17 @@
 import styles from './Header.module.scss';
+import Image from 'next/image';
 
-const Header = () => <header className={styles.header}>DUMMY HEADER</header>;
+const Header = () => (
+  <header className={styles.header}>
+    <Image src="/assets/svg/header-logo.svg" alt="Logo" width={74} height={24} priority />
+    <Image
+      src="/assets/svg/shopping-cart.svg"
+      alt="Shopping cart"
+      width={18}
+      height={18}
+      priority
+    />
+  </header>
+);
 
 export default Header;
