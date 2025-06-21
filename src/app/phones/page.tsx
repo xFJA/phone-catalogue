c3a43313e1b4f48b2af435aabd0d0a2379a280a8
xfja <franjimenezaguilera@gmail.com>
2025-06-21 18:42:18 +0200

    fix: set init phones number to 20

--- src/app/phones/page.tsx
+++ src/app/phones/page.tsx
@@ -3,7 +3,7 @@ import { phoneService } from '@/services/phoneService';
 import PhoneSearch from '@/components/PhoneSearch';
 
 export default async function PhonesPage() {
-  const initialPhones = await phoneService.getAllPhones();
+  const initialPhones = await phoneService.getAllPhones(20, 0);
 
   return (
     <main className={styles['phones-page']}>
