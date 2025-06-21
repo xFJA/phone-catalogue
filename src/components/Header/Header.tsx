45b858417bac1fb8253ffea288b2224cdf910c53
xfja <franjimenezaguilera@gmail.com>
2025-06-21 21:28:35 +0200

    fix: hide ShoppinCart on /cart page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
+'use client';
+
 import styles from './Header.module.scss';
 import Image from 'next/image';
 import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-const Header = () => (
-  <header className={styles.header}>
-    <Link href="/">
-      <Image src="/assets/svg/header-logo.svg" alt="Logo" width={74} height={24} priority />
-    </Link>
-    <ShoppingCart />
-  </header>
-);
+const Header = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <Image src="/assets/svg/header-logo.svg" alt="Logo" width={74} height={24} priority />
+      </Link>
+      {pathname !== '/cart' && <ShoppingCart />}
+    </header>
+  );
+};
 
 export default Header;
