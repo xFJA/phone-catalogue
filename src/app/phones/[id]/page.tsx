4c5b9de1a6b59e00dc0c2a2e1102da1233db46ed
xfja <franjimenezaguilera@gmail.com>
2025-06-21 13:25:23 +0200

    feat: add Specifications section

--- src/app/phones/[id]/page.tsx
+++ src/app/phones/[id]/page.tsx
@@ -1,24 +1,78 @@
 import { phoneService } from '@/services/phoneService';
 import { notFound } from 'next/navigation';
 import PhoneDetail from '@/components/PhoneDetail';
+import styles from './PhoneDetailPage.module.scss';
 
 interface PhoneDetailPageProps {
   params: { id: string };
 }
 
 export default async function PhoneDetailPage({ params }: PhoneDetailPageProps) {
-  const { id } = await params;
+  const { id } = params;
   const phone = await phoneService.getPhoneById(id);
   if (!phone) return notFound();
 
-  const { name, basePrice, colorOptions, storageOptions } = phone;
+  const { name, basePrice, colorOptions, storageOptions, brand, specs, description } = phone;
   return (
-    <PhoneDetail
-      id={id}
-      name={name}
-      basePrice={basePrice}
-      colorOptions={colorOptions}
-      storageOptions={storageOptions}
-    />
+    <div>
+      <PhoneDetail
+        id={id}
+        name={name}
+        basePrice={basePrice}
+        colorOptions={colorOptions}
+        storageOptions={storageOptions}
+      />
+      <div className={styles['specifications']}>
+        <h2 className={styles['specifications__title']}>SPECIFICATIONS</h2>
+        <table className={styles['specifications__table']}>
+          <tbody>
+            <tr>
+              <th>BRAND</th>
+              <td>{brand}</td>
+            </tr>
+            <tr>
+              <th>NAME</th>
+              <td>{name}</td>
+            </tr>
+            <tr>
+              <th>DESCRIPTION</th>
+              <td>{description}</td>
+            </tr>
+            <tr>
+              <th>SCREEN</th>
+              <td>{specs.screen}</td>
+            </tr>
+            <tr>
+              <th>RESOLUTION</th>
+              <td>{specs.resolution}</td>
+            </tr>
+            <tr>
+              <th>PROCESSOR</th>
+              <td>{specs.processor}</td>
+            </tr>
+            <tr>
+              <th>MAIN CAMERA</th>
+              <td>{specs.mainCamera}</td>
+            </tr>
+            <tr>
+              <th>SELFIE CAMERA</th>
+              <td>{specs.selfieCamera}</td>
+            </tr>
+            <tr>
+              <th>BATTERY</th>
+              <td>{specs.battery}</td>
+            </tr>
+            <tr>
+              <th>OS</th>
+              <td>{specs.os}</td>
+            </tr>
+            <tr>
+              <th>SCREEN REFRESH RATE</th>
+              <td>{specs.screenRefreshRate}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
