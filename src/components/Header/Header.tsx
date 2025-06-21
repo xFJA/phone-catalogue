8117c7cd8b9e251bc715e6b37ecfebefb9c3c013
xfja <franjimenezaguilera@gmail.com>
2025-06-21 13:06:27 +0200

    feat: add redirect to main page for Header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import styles from './Header.module.scss';
 import Image from 'next/image';
 import ShoppingCart from '../ShoppingCart/ShoppingCart';
+import Link from 'next/link';
 
 const Header = () => (
   <header className={styles.header}>
-    <Image src="/assets/svg/header-logo.svg" alt="Logo" width={74} height={24} priority />
+    <Link href="/">
+      <Image src="/assets/svg/header-logo.svg" alt="Logo" width={74} height={24} priority />
+    </Link>
     <ShoppingCart />
   </header>
 );
